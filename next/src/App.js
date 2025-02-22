c644cca48e3733b0d5a507d60b365df2c772e452
nithish0302 <nithi2005palani@gmail.com>
2025-02-22 22:23:21 +0530

    File modified

--- next/src/App.js
+++ next/src/App.js
@@ -1,50 +1,63 @@
 // import logo from './logo.svg';
 import React from "react";
-import { useState } from 'react';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 const messages = [
   "Learn React ⚛️",
   "Apply for jobs 💼",
   "Invest your new income 🤑",
 ];
 
-
 function App() {
-  const [step,setStep]=useState(1);
-  let [isOpen,setisOpen]=useState(true);
-  function previous()
-  {
-    if(step>=2)
-    {
-      setStep(step-1);
+  const [step, setStep] = useState(1);
+  let [isOpen, setisOpen] = useState(true);
+  function previous() {
+    if (step >= 2) {
+      setStep(step - 1);
     }
   }
-  function next()
-  {
-    if(step<=2)
-    {
-      setStep(step+1);
+  function next() {
+    if (step <= 2) {
+      setStep(step + 1);
     }
   }
-  return <div>
+  return (
+    <div>
+      <button className="close" onClick={() => setisOpen((is) => !is)}>
+        &times;
+      </button>
+      <div>
+        {isOpen && (
+          <div className="steps">
+            <div className="numbers">
+              <div className={step === 1 ? "active" : ""}>1</div>
+              <div className={step === 2 ? "active" : ""}>2</div>
+              <div classNae={step === 3 ? "active" : ""}>3</div>
+            </div>
 
-    <button className="close" onClick={()=>setisOpen(!isOpen)}>&times;</button>
-   <div>   
-   {isOpen &&( <div className='steps'>
-      <div className='numbers'>
-        <div className={step===1?'active':''}>1</div>
-        <div className={step===2?'active':''}>2</div>
-        <div className={step===3?'active':''}>3</div>
-      </div>
+            <p className="message">
+              Step:{step} : {messages[step - 1]}
+            </p>
 
-      <p className='message' >Step:{step} : {messages[step-1]}</p>
-
-      <div className='buttons'>
-        <button style={{backgroundColor:'green', color:'#ffff' }} onClick={previous}>Previous</button>
-        <button style={{backgroundColor:'green', color:'#ffff' }} onClick={next}>Next</button>
-        </div>
+            <div className="buttons">
+              <button
+                style={{ backgroundColor: "green", color: "#ffff" }}
+                onClick={previous}
+              >
+                Previous
+              </button>
+              <button
+                style={{ backgroundColor: "green", color: "#ffff" }}
+                onClick={next}
+              >
+                Next
+              </button>
+            </div>
+          </div>
+        )}
+      </div>{" "}
     </div>
-)}</div> </div>
+  );
 }
 
 export default App;
