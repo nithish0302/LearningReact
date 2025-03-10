9cd148a8d1bee80f7450406e22af9f69246159e3
nithish0302 <nithi5palani@gmail.com>
2025-03-10 12:03:09 +0530

    File modified

--- next/src/App.js
+++ next/src/App.js
@@ -22,6 +22,7 @@ export default function App() {
   }
   return (
     <div>
+      <h4>HI</h4>
       <button className="close" onClick={() => setisOpen((is) => !is)}>
         &times;
       </button>
